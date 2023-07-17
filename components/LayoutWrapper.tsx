5bcd0faf094a2b9983b731f1100953c43ff04126
Karhdo <dotrongkhanh.dev@gmail.com>
2023-07-17 22:55:22 +0700

    chore: Revert font default

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -1,17 +1,21 @@
-import { ReactNode } from 'react';
-
-import Footer from './Footer';
-import Header from './Header';
+import { Inter } from '@next/font/google';
 import SectionContainer from './SectionContainer';
+import Footer from './Footer';
+import { ReactNode } from 'react';
+import Header from './Header';
 
 interface Props {
   children: ReactNode;
 }
 
+const inter = Inter({
+  subsets: ['latin'],
+});
+
 const LayoutWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
+      <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
         <Header />
         <main className="mb-auto">{children}</main>
         <Footer />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,17 +15,6 @@ class MyDocument extends Document {
           <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
           <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body className="bg-white text-black antialiased dark:bg-dark dark:text-white">
           <Main />
