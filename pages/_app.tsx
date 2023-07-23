e5b5c16c89f8dd21d596735775ff4930d0e2eef8
Karhdo <dotrongkhanh.dev@gmail.com>
2023-07-23 13:20:57 +0700

    chore(app): format file _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
-import '@/css/tailwind.css'
-import '@/css/prism.css'
-import 'katex/dist/katex.css'
+import '@/css/tailwind.css';
+import '@/css/twemoji.css';
+import '@/css/prism.css';
+import 'katex/dist/katex.css';
 // import '@/css/docsearch.css' // Uncomment if using algolia docsearch
 // import '@docsearch/css' // Uncomment if using algolia docsearch
 
-import { ThemeProvider } from 'next-themes'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import { ThemeProvider } from 'next-themes';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
-import siteMetadata from '@/data/siteMetadata'
-import { Analytics } from 'pliny/analytics'
-import { SearchProvider } from 'pliny/search'
-import LayoutWrapper from '@/components/LayoutWrapper'
+import siteMetadata from '@/data/siteMetadata';
+import { Analytics } from 'pliny/analytics';
+import { SearchProvider } from 'pliny/search';
+import LayoutWrapper from '@/components/LayoutWrapper';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -26,5 +27,5 @@ export default function App({ Component, pageProps }: AppProps) {
         </SearchProvider>
       </LayoutWrapper>
     </ThemeProvider>
-  )
+  );
 }
