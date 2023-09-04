b156b4a06cda12e658e8cba7422edaddbf48311c
Karhdo <dotrongkhanh.dev@gmail.com>
2023-09-04 23:51:49 +0700

    feat(about): Setup about page

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -15,12 +15,17 @@ export default function AuthorLayout({ children, content }: Props) {
   return (
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
+
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             About
           </h1>
+          <p className="text-base text-gray-500 dark:text-gray-400 md:text-lg md:leading-7">
+            Further insights into who I am and the purpose of this blog.
+          </p>
         </div>
+
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 pt-8">
             <Image
@@ -33,12 +38,6 @@ export default function AuthorLayout({ children, content }: Props) {
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
-            <div className="flex space-x-3 pt-6">
-              <SocialIcon kind="mail" href={`mailto:${email}`} />
-              <SocialIcon kind="github" href={github} />
-              <SocialIcon kind="linkedin" href={linkedin} />
-              <SocialIcon kind="twitter" href={twitter} />
-            </div>
           </div>
           <div className="prose max-w-none pt-8 pb-8 dark:prose-dark xl:col-span-2">{children}</div>
         </div>
