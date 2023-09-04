3bafe3caa21e44a51478f3c67d9e1084755dca2f
Karhdo <dotrongkhanh.dev@gmail.com>
2023-09-04 23:51:49 +0700

    feat(about): Setup about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,12 +2,14 @@
 import { InferGetStaticPropsType } from 'next';
 import { allAuthors } from 'contentlayer/generated';
 import { MDXLayoutRenderer } from 'pliny/mdx-components';
+
 import { MDXComponents } from '@/components/MDXComponents';
 
 const DEFAULT_LAYOUT = 'AuthorLayout';
 
 export const getStaticProps = async () => {
   const author = allAuthors.find((p) => p.slug === 'default');
+
   return { props: { author } };
 };
 
