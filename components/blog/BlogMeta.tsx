aea95bd76f38fed334fbd0ad5cf20589b05ed9e7
Karhdo <dotrongkhanh.dev@gmail.com>
2023-09-22 07:50:14 +0700

    feat(blog): Build api counter views

--- components/blog/BlogMeta.tsx
+++ components/blog/BlogMeta.tsx
@@ -7,17 +7,15 @@ import ViewCounter from './ViewCounter';
 
 const BlogMeta = ({ date, slug, readingTime }: BlogMetaProps) => {
   return (
-    <dd className='flex flex-column gap-1 font-semibold text-gray-500 dark:text-gray-400'>
-      <time dateTime={date} className='flex items-center'>
-        <Twemoji emoji='calendar' size='1' />
-        <span className='ml-1 md:ml-2'>{formatDate(date)}</span>
+    <dd className="flex-column flex gap-1 font-semibold text-gray-500 dark:text-gray-400">
+      <time dateTime={date} className="flex items-center">
+        <Twemoji emoji="calendar" size="1" />
+        <span className="ml-1 md:ml-2">{formatDate(date)}</span>
       </time>
       <span className="mx-2">{` • `}</span>
       <div className="flex items-center">
-        <Twemoji emoji="hourglass-not-done" size='1' />
-        <span className="ml-1.5 md:ml-2">
-          {Math.ceil(readingTime.minutes)} mins read
-        </span>
+        <Twemoji emoji="hourglass-not-done" size="1" />
+        <span className="ml-1.5 md:ml-2">{Math.ceil(readingTime.minutes)} mins read</span>
       </div>
       <span className="mx-2">{` • `}</span>
       <div className="flex items-center">
@@ -25,7 +23,7 @@ const BlogMeta = ({ date, slug, readingTime }: BlogMetaProps) => {
         <ViewCounter className="ml-1.5 md:ml-2" slug={slug} />
       </div>
     </dd>
-  )
-}
+  );
+};
 
 export default BlogMeta;
---
+++ pages/api/views/index.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+import prisma from '@/libs/prisma.server';
+
+export default async function handler(_: NextApiRequest, res: NextApiResponse) {
+  try {
+    const totalViews = await prisma.views.aggregate({
+      _sum: {
+        count: true,
+      },
+    });
+
+    return res.status(200).json({ total: totalViews._sum.count?.toString?.() || 0 });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
