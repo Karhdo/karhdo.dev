d8c83fad80ef7389e8b2775864bd3e80634ca9cc
khanhdt <khanhdt@younetgroup.com>
2023-09-25 16:26:20 +0700

    feat(header): Update position component header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,36 +14,38 @@ const Header = () => {
   const router = useRouter();
 
   return (
-    <header className="supports-backdrop-blur sticky top-0 z-40 flex items-center justify-between bg-white/75 py-4 backdrop-blur dark:bg-dark/75">
-      <Link href="/" aria-label={siteMetadata.headerTitle} className="flex items-center">
-        <div className="animate-wave">
-          <Logo className="fill-dark dark:fill-white" />
+    <header className="supports-backdrop-blur fixed left-0 right-0 top-0 z-40 bg-white/75 py-4 backdrop-blur dark:bg-dark/75">
+      <div className="mx-auto flex max-w-4xl items-center justify-between px-3 xl:max-w-6xl xl:px-0">
+        <Link href="/" aria-label={siteMetadata.headerTitle} className="flex items-center">
+          <div className="animate-wave">
+            <Logo className="fill-dark dark:fill-white" />
+          </div>
+          <div className="group ml-2 text-xl font-bold transition duration-300">
+            Karhdo.dev
+            <span className="block h-0.5 max-w-0 bg-black transition-all duration-500 group-hover:max-w-[85%] dark:bg-white"></span>
+          </div>
+        </Link>
+        <div className="flex items-center text-base leading-5">
+          <div className="hidden sm:block">
+            {headerNavLinks.map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className={clsx(
+                  'mx-1 rounded px-2 py-1 font-medium text-gray-900 dark:text-gray-100 sm:px-3 sm:py-2',
+                  router.pathname.startsWith(link.href)
+                    ? 'bg-gray-200 dark:bg-primary'
+                    : 'hover:bg-gray-200 dark:hover:bg-primary'
+                )}
+                data-umami-event={`nav-${link.href.replace('/', '')}`}
+              >
+                {link.title}
+              </Link>
+            ))}
+          </div>
+          <ThemeSwitch />
+          <MobileNav />
         </div>
-        <div className="group ml-2 text-xl font-bold transition duration-300">
-          Karhdo.dev
-          <span className="block h-0.5 max-w-0 bg-black transition-all duration-500 group-hover:max-w-[85%] dark:bg-white"></span>
-        </div>
-      </Link>
-      <div className="flex items-center text-base leading-5">
-        <div className="hidden sm:block">
-          {headerNavLinks.map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className={clsx(
-                'mx-1 rounded px-2 py-1 font-medium text-gray-900 dark:text-gray-100 sm:px-3 sm:py-2',
-                router.pathname.startsWith(link.href)
-                  ? 'bg-gray-200 dark:bg-primary'
-                  : 'hover:bg-gray-200 dark:hover:bg-primary'
-              )}
-              data-umami-event={`nav-${link.href.replace('/', '')}`}
-            >
-              {link.title}
-            </Link>
-          ))}
-        </div>
-        <ThemeSwitch />
-        <MobileNav />
       </div>
     </header>
   );
--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -5,5 +5,5 @@ interface Props {
 }
 
 export default function SectionContainer({ children }: Props) {
-  return <section className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</section>;
+  return <section className="mx-auto max-w-4xl px-4 sm:px-6 xl:max-w-6xl xl:px-0">{children}</section>;
 }
