1c583554a938be514f6a755c209d5ffc25cb2d38
Karhdo <dotrongkhanh.dev@gmail.com>
2023-10-24 22:52:01 +0700

    chore(app): Remove .env and prettier

--- components/blog/BlogTags.tsx
+++ components/blog/BlogTags.tsx
@@ -2,16 +2,16 @@ import Tag from '../Tag';
 
 const BlogTags = ({ tags }: { tags: string[] }) => {
   if (!tags || tags.length === 0) {
-    return null
+    return null;
   }
 
   return (
-    <div className='flex flex-wrap !mt-1'>
+    <div className="!mt-1 flex flex-wrap">
       {tags.map((tag) => (
         <Tag key={tag} text={tag} />
       ))}
     </div>
-  )
-}
+  );
+};
 
 export default BlogTags;
