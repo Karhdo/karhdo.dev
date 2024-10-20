23035d16795174511c6f807aa2f4585dfb47c802
khanhdt <khanhdt@younetgroup.com>
2024-10-20 19:33:18 +0700

    feat: Init version 2

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -2,14 +2,19 @@ import { ReactNode } from 'react';
 import type { Authors } from 'contentlayer/generated';
 import { Mail, Linkedin, Github, Twitter } from 'lucide-react';
 
-import Image from '@/components/ui/Image';
 import Link from '@/components/ui/Link';
+import Image from '@/components/ui/Image';
+import Button from '@/components/ui/Button';
+import Twemoji from '@/components/ui/Twemoji';
+
+import CareerTimeline from '@/components/about/CareerTimeline';
 
 interface Props {
   children: ReactNode;
   content: Omit<Authors, '_id' | '_raw' | 'body'>;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function AuthorLayout({ children, content }: Props) {
   const { name, avatar, occupation, company, email, twitter, linkedin, github } = content;
 
@@ -49,7 +54,51 @@ export default function AuthorLayout({ children, content }: Props) {
             </div>
           </div>
 
-          <div className="prose max-w-none pb-8 dark:prose-dark xl:col-span-2">{children}</div>
+          {/* <div className="prose max-w-none pb-8 dark:prose-dark xl:col-span-2">{children}</div> */}
+
+          <div className="prose max-w-none pb-8 dark:prose-dark xl:col-span-2">
+            <h2>
+              Hello, folks! <Twemoji className="mx-2" emoji="waving-hand" /> I'm Trong Khanh (aka Karhdo)
+            </h2>
+
+            <p>
+              I have a passion for <strong>JavaScript/TypeScript</strong> and website development. I'm currently a
+              fullstack developer at <strong>YouNet Media</strong>, deeply engaged in the <strong>EcomHeat</strong>{' '}
+              project, which entails market share management, competitor research, and fostering e-commerce growth. I
+              work mainly with <strong>JavaScript</strong>, <strong>TypeScript</strong>, <strong>React</strong>,{' '}
+              <strong>NodeJS</strong>, <strong>NestJS</strong>, and <strong>NextJS</strong>.
+            </p>
+
+            <h2>Why have this blog?</h2>
+
+            <blockquote>
+              <p>My desire to practice my skills and share my acquired knowledge fuels my endeavors.</p>
+            </blockquote>
+
+            <p>
+              I founded this blog as a means to document and share the knowledge and practical wisdom I've acquired
+              during my journey as a software engineer.
+            </p>
+            <p>
+              Writing and taking notes helps me solidify my understanding of new concepts and technologies. I hope my
+              blog can be a useful resource for fellow web developers.
+            </p>
+            <p>
+              I would greatly appreciate your thoughts and comments on what I have written{' '}
+              <Twemoji emoji="clinking-beer-mugs" />.
+            </p>
+
+            <div className="flex items-center justify-between">
+              <h2>My Career</h2>
+
+              <Button href="/static/resume.pdf" target="_blank">
+                <span>Resume</span>
+                <Twemoji emoji="page-facing-up" />
+              </Button>
+            </div>
+
+            <CareerTimeline />
+          </div>
         </div>
       </div>
     </>
